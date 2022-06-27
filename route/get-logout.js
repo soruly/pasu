bddb48ee1f14f220d36b5017ddb46545f10f1503
soruly <soruly@gmail.com>
2022-06-27 10:51:28 +0000

    Remove server-side session when logging out

--- route/get-logout.js
+++ route/get-logout.js
@@ -1,7 +1,10 @@
+import fs from "fs-extra";
+
 const { ENABLE_FIDO2 } = process.env;
 
 export default async (req, res) => {
   if (!ENABLE_FIDO2) return res.status(403).send("FIDO2 disabled");
+  fs.removeSync(`session/${req.cookies.session}.json`);
   res.setHeader(
     "Set-Cookie",
     `session=; Path=/; Expires=${new Date(0).toGMTString()}; Secure; HttpOnly; SameSite=Strict`
