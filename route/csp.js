c7fd3dc2cabac29df42189ef97758cc3321b70e6
soruly <soruly@gmail.com>
2025-03-13 19:29:36 +0000

    Update security header

--- route/csp.js
+++ route/csp.js
@@ -1,14 +1,22 @@
 export default async (req, res, next) => {
-  res.set("Access-Control-Allow-Origin", "*");
-  res.set("Access-Control-Allow-Methods", "GET, OPTIONS");
   res.set("Referrer-Policy", "no-referrer");
   res.set("X-Content-Type-Options", "nosniff");
+  res.set("Cross-Origin-Resource-Policy", "same-origin");
   res.set(
     "Content-Security-Policy",
     [
-      "default-src 'self'",
-      "base-uri 'none'",
+      "default-src 'none'",
       "frame-ancestors 'none'",
+      "base-uri 'none'",
+      "style-src 'self'",
+      "img-src 'self'",
+      "font-src * 'self'",
+      "script-src 'self'",
+      "connect-src 'self'",
+      "form-action 'self'",
+      "media-src 'self'",
+      "manifest-src 'self'",
+      "worker-src 'self'",
       "block-all-mixed-content",
     ].join("; "),
   );
